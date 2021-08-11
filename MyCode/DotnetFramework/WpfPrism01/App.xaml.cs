5715ec602fd339a24f556a71148cf24b92aa898f
Wagner Xu <wagnerhsu@hotmail.com>
2021-08-11 14:07:38 +0800

    Add WpfPrism01

---
+++ MyCode/DotnetFramework/WpfPrism01/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+using Unity;
+
+namespace WpfPrism01
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            var w = Container.Resolve<MainWindow>();
+            return w;
+        }
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+    }
+}
---
+++ MyCode/DotnetFramework/WpfPrism01/README.md
@@ -0,0 +1,6 @@
+﻿## 2021-08-11
+- Create this file based on .NET 6.0 Application with the following changes
+	- Change `TargetFramework` from `net6.0-windows` to `net461`
+	- Modify csproj to remove `<Nullable>enable</Nullable>`
+- Add reference `Prism.Unity`
+- Modify App.xmal and App.xaml.cs
