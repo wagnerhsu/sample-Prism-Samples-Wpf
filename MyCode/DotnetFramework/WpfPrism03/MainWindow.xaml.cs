97c940d16f8438a978739a4e4f5f63d23d24b854
Wagner Xu <wagnerhsu@hotmail.com>
2021-08-12 15:28:48 +0800

    Add WpfPrism03

---
+++ MyCode/DotnetFramework/WpfPrism03/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+using System.Windows;
+
+namespace WpfPrism03
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow(ILogger<MainWindow> logger)
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyCode/DotnetFramework/WpfPrism03/README.md
@@ -0,0 +1,2 @@
+﻿## 2021-08-12
+- Currently there is a problem to call AddNLog extension method
